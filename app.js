f796c467f396c89f9431a6824a82ba87f564a1c7
Ilgorbek Abdumalikov <ilgorbekabdumalikov331@gmail.com>
2022-09-25 15:57:03 +0500

    changed bot

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import TelegramBot  from 'node-telegram-bot-api';
+import dotenv from 'dotenv'
 
-const token = '5680804675:AAG0We22XZUzVOH2h2NmijaaEIgddk85xI8';
+dotenv.config()
+
+const token = process.env.token;
 
 const bot = new TelegramBot(token, {polling: true});
 
@@ -15,12 +18,13 @@ const objSherik = {
     callTime:'',
     goal:''
 }
-
-const arr = ['category', 'allName', 'technology', 'call', 'region', 'salary', 'jobs', 'callTime', 'goal']
 var chatId ;
 
-bot.on('message', (msg) => {
+var step = 0;
 
+bot.on('message', (msg) => {
+   
+    console.log(msg);
     // started bot
      if(msg.text === '/start'){
         const opts = {
@@ -38,8 +42,11 @@ bot.on('message', (msg) => {
             message,
             opts,
          )
+         if(step === 0 ){
+            step++
         }
-  
+         return
+        }
 
      //category Sherik kerak
      if(msg.text === 'Sherik kerak'){
@@ -51,28 +58,134 @@ bot.on('message', (msg) => {
             {parse_mode:'HTML'}
             )
         bot.sendMessage(msg.chat.id, '<b>Isim, familiyangizni kiriting</b>', {parse_mode:'HTML'})
-     }
-     
-     if(msg.message_id - chatId == 3 ){
-        objSherik.allName = msg.text
-        chatId = msg.message_id
-        console.log(chatId);
-        bot.sendMessage(msg.chat.id, `
-        📚 <b>Texnologiya:</b> \n\nTalab qilinadigan texnologiyalarni kiriting \nTexnologiya nomlarini vergul bilan ajrating. Masalan,\n\nJava, C++, C#`,
-        {parse_mode:'HTML'}
-        )
+        if(step === 1){
+            step++
+        }
+        return
      }
 
-    if(msg.message_id-chatId == 2){
+     if(step===2 ){
         objSherik.allName = msg.text
         chatId = msg.message_id
-        bot.sendMessage(msg.chat.id, 
-            ` 📞 <b>Aloqa:</b> \n\nBog'lanish uchun raqamingizni kiriting \nMasalan, +998 90 123 45 67 `,
+        bot.sendMessage(
+            msg.chat.id, 
+            `<strong>📚 Texnologiya:</strong> \n\nTalab qilinadigan texnologiyalarni kiriting \nTexnologiya nomlarini vergul bilan ajrating. Masalan, \n\nJava, C++, C#.`,
             {parse_mode:'HTML'}
             )
+            if(step === 2){
+                step++
+            }
+            return
+     }
+     
+     if(step===3){
+        objSherik.technology = msg.text
+        chatId = msg.message_id
+        bot.sendMessage(
+            msg.chat.id, 
+            `<strong>📞 Aloqa:</strong> \n\nBog'lanish uchun raqamingizni kiriting?\nMasalan, +998 90 123 45 67`,
+            {parse_mode:'HTML'}
+            )
+            if(step === 3){  
+                step++
+            }
+            return
+     }
+
+     if(step===4){
+        objSherik.call = msg.text
+        chatId = msg.message_id
+        bot.sendMessage(
+            msg.chat.id, 
+            `<strong>🌐 Hudud:</strong> \n\nQaysi hududdansiz?\nViloyat nomi, Toshkent shahar yoki Respublikani kiriting.`,
+            {parse_mode:'HTML'}
+            )
+           if(step ===4){
+            step++
+           }
+            return
+     }
+
+     if(step === 5){
+        objSherik.region = msg.text
+        chatId = msg.message_id
+        bot.sendMessage(
+            msg.chat.id, 
+            `<strong>💰 Narxi:</strong> \n\nTolov qilasizmi yoki Tekinmi?\nKerak bo'lsa, Summani kiriting?`,
+            {parse_mode:'HTML'}
+            )
+            if(step ===5){
+                step++
+               }
+            return
+     }
+
+     if(step===6){
+        objSherik.salary = msg.text
+        chatId = msg.message_id
+        bot.sendMessage(msg.chat.id, 
+            `<b>👨🏻‍💻 Kasbi: </b> \n\nIshlaysizmi yoki o'qiysizmi?
+            \nMasalan, Talaba`,
+            {parse_mode:'HTML'}
+            )
+            if(step ===6){
+                step++
+               }
+            return
     }
 
+    if(step===7){
+        objSherik.jobs = msg.text
+        chatId = msg.message_id
+        bot.sendMessage(msg.chat.id, 
+            `<b>🕰 Murojaat qilish vaqti:</b> \n\nQaysi vaqtda murojaat qilish mumkin?
+            \nMasalan, 9:00 - 18:00`,
+            {parse_mode:'HTML'}
+            )
+        if(step ===7){
+            step++
+        }
+            return
+    }
+
+    if(step===8){
+        objSherik.callTime = msg.text
+        chatId = msg.message_id
+        bot.sendMessage(msg.chat.id, 
+            `<b>🔎 Maqsad:</b> \n\nMaqsadingizni qisqacha yozib bering.`,
+            {parse_mode:'HTML'}
+            )
+            if(step ===8){
+                step++
+               }
+            return
+    }
+    
+    if(step===9){
+        objSherik.goal === msg.text
+
+        const message = `
+           \n\n<b>${objSherik.category}:</b>
+           \n\n🏅 Sherik: ${objSherik.allName},\n📚 Texnologiya: ${objSherik.technology},\n🇺🇿 Telegram: ${msg.from.username},\n📞 Aloqa: ${objSherik.call},\n🌐 Hudud: ${objSherik.region},\n💰 Narxi: ${objSherik.salary},\n👨🏻‍💻 Kasbi: ${objSherik.jobs},\n🕰 Murojaat qilish vaqti: ${objSherik.callTime},\n🔎 Maqsad: ${objSherik.goal}\n\n${objSherik.technology},${objSherik.region}\n\n
+           `
+
+           const opts = {
+            reply_markup:{
+                keyboard: [
+                    ['Ha', 'Yo`q']  
+                ],
+            }
+        } 
+
+        bot.sendMessage(
+            msg.chat.id,
+            message,
+            {parse_mode:'HTML'}
+        )
+        bot.sendMessage(msg.chat.id, `Barcha ma'lumotlar to'g'rimi?`, opts)
+    }
 
 });
+
 
 
